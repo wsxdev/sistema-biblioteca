e2b0794a6145560d92d7833cef319e166c916f51
Britanr <redrovanbritay1751623776gmail.com>
2025-10-20 21:38:52 -0500

    Agregando Definicion al record Tarjeta

--- src/main/java/com/biblioteca/modelo/entidad/Tarjeta.java
+++ src/main/java/com/biblioteca/modelo/entidad/Tarjeta.java
@@ -3,6 +3,10 @@ package com.biblioteca.modelo.entidad;
 import com.biblioteca.modelo.enumeracion.TipoTarjeta;
 import com.biblioteca.modelo.enumeracion.ZonaLectura;
 
-public record Tarjeta(String prefijoCodigo, ZonaLectura zonaLectura, TipoTarjeta tipoTarjeta, int limitePrestamos) {
+public record Tarjeta(String prefijoCodigo, ZonaLectura  zonaLectura, TipoTarjeta tipoTarjeta, int limitePrestamos) {
+    
+    public int consultarLimitePrestamos(){
+        return limitePrestamos;
+    }
     
 }
