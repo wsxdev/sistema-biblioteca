c91b105253e3aa1984c47e5a4964ce4afdd0ce1e
licetdev <marialiset13@gmail.com>
2025-11-24 17:01:15 -0500

    Refactor code structure for improved readability and maintainability

--- src/main/java/com/biblioteca/modelo/implementacion/EstanteriaDAO.java
+++ src/main/java/com/biblioteca/modelo/implementacion/EstanteriaDAO.java
@@ -4,19 +4,22 @@ import com.biblioteca.modelo.interfaz.IEstanteriaDAO;
 
 import com.biblioteca.modelo.entidad.*;
 public class EstanteriaDAO implements IEstanteriaDAO {
-
+    
+    @Override
     public boolean agregarLibros(Libro libro){
-
+        
         return true;
     }
+    @Override
     public Libro[] buscarLibroPorCompartimento(int codigo){
 
         return null;
     }
+    @Override
     public void listarPosicionesLibres(){
 
     }
-
+    @Override
     public void listarPosicionesOcupadas(){
         
     }
